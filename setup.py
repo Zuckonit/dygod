d787c883c7bbc191108ea711131d10fa27a926d0
Mocker <lsin30@foxmail.com>
2018-01-13 21:21:40 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dygod',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Click',
+        'requests'
+    ],
+    entry_points='''
+        [console_scripts]
+        dygod=dygod.cmd:cli
+    ''',
+)
